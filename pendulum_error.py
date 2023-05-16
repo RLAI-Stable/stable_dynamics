Fixed precision issues on error estimation

pendulum_error.py
@@ -78,18 +78,17 @@ def main(args):
         # TODO: Update error calculation
         start = time.time()
         vel_error = np.sum((X_phy[i,:,n:] - y[:,n:])**2)
-        ang_error = (X_phy[i,:,:n] - y[:,:n])
+        ang_error = (X_phy[i,:,:n] - y[:,:n]).astype('float64')
         print(f"{i}: Time to calculate error: {time.time()-start}")
 
         start = time.time()
-        # while np.any(ang_error >= np.pi):
-        #     ang_error[ang_error >= np.pi] -= 2*np.pi
-        # while np.any(ang_error < -np.pi):
-        #     ang_error[ang_error < -np.pi] += 2*np.pi
+        # Scales errors to the range [0, 2pi]
         mod = ang_error//(2*np.pi)
         ang_error -= mod*2*np.pi
+
+        # Then moves them to the range [-pi, pi]
         ang_error -= np.pi
-        assert all(np.all(ang_error <= np.pi), np.all(ang_error >= -np.pi))
+        assert all([np.all(ang_error <= np.pi), np.all(ang_error >= -np.pi)])
         print(f"{i}: Time to wrap error: {time.time()-start}")
 
         start = time.time()