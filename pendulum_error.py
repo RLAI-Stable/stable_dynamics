Bugfix: Scaling

pendulum_error.py
@@ -82,13 +82,14 @@ def main(args):
         print(f"{i}: Time to calculate error: {time.time()-start}")
 
         start = time.time()
-        # Scales errors to the range [0, 2pi]
+        # Scales errors to the range [0, 2pi)
         mod = ang_error//(2*np.pi)
         ang_error -= mod*2*np.pi
 
-        # Then moves them to the range [-pi, pi]
-        ang_error -= np.pi
-        assert all([np.all(ang_error <= np.pi), np.all(ang_error >= -np.pi)])
+        # Then moves them to the range (-pi, pi]
+        if np.any(ang_error > np.pi):
+            ang_error[ang_error > np.pi] -= 2*np.pi
+        assert all([np.all(ang_error <= np.pi), np.all(ang_error >= -np.pi)]), f"Angles not in range: {ang_error[ang_error > np.pi], ang_error[ang_error < -np.pi]}"
         print(f"{i}: Time to wrap error: {time.time()-start}")
 
         start = time.time()