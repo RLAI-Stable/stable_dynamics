c27f557592319e106839a6f135c719ececa2844e
kailashs2002 <kailashseshadri2002@gmail.com>
2023-05-26 21:27:36 -0600

    Updated saving, added filepath arg to bash files

--- pendulum_error.py
+++ pendulum_error.py
@@ -65,6 +65,9 @@ def main(args):
     X_nn = to_variable(torch.tensor(X_phy[0,:,:]), cuda=torch.cuda.is_available())
     errors = np.zeros((args.steps,))
     for i in range(1, args.steps):
+        logger.info(f"Timestep {i}")
+
+        # Generate prediction
         X_nn.requires_grad = True
         k1 = h * model(X_nn)
         k1 = k1.detach()
@@ -76,22 +79,68 @@ def main(args):
         k4 = k4.detach()
         X_nn = X_nn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
         X_nn = X_nn.detach()
-
-        logger.info(f"Timestep {i}")
         y = X_nn.cpu().numpy()
+
         # TODO: Update error calculation
         vel_error = np.sum((X_phy[i,:,n:] - y[:,n:])**2)
-        ang_error = (X_phy[i,:,:n] - y[:,:n])
-        while np.any(ang_error >= np.pi):
-            ang_error[ang_error >= np.pi] -= 2*np.pi
-        while np.any(ang_error < -np.pi):
-            ang_error[ang_error < -np.pi] += 2*np.pi
+        ang_error = (X_phy[i,:,:n] - y[:,:n]).astype('float64')
+        ang_error = scaleAngErr(ang_error)
 
         ang_error = np.sum(ang_error**2)
+
         errors[i] = (vel_error + ang_error)
 
-    for i in range(args.steps):
-        print(f"{i}\t{np.sum(errors[0:i])}\t{errors[i]}")
+    filename = genFilename(args)
+    np.save(filename+".npy", errors)
+    generateplot(args, filename, errors)
+
+    # for i in range(args.steps):
+    #     print(f"{i}\t{np.sum(errors[0:i])}\t{errors[i]}"
+    print(f"Avg error: {np.sum(errors)/args.steps}")
+
+
+
+
+def scaleAngErr(ang_error):
+    # Scales errors to the range [0, 2pi)
+    mod = ang_error//(2*np.pi)
+    ang_error -= mod*2*np.pi
+    # assert all([np.all(ang_error <= 2*np.pi), np.all(ang_error >= 0)]), f"Angles not in range [0, 2pi]: {ang_error[ang_error > 2*np.pi], ang_error[ang_error < 0]}"
+
+    # Then moves them to the range (-pi, pi]
+    if np.any(ang_error > np.pi):
+        ang_error[ang_error > np.pi] -= 2*np.pi
+    assert all([np.all(ang_error <= np.pi), np.all(ang_error >= -np.pi)]), f"Angles not in range [-pi, pi]: {ang_error[ang_error > np.pi], ang_error[ang_error < -np.pi]}"
+
+    return ang_error
+
+
+def genFilename(args):
+    from os.path import exists as path_exists
+
+    if args.output.endswith("/") or args.output.endswith("\\") or args.output == "":
+        default_fname = fname = f"pendulum-error-{args.data._n}"
+
+        i = 0
+        while path_exists(args.output+fname+".npy") or path_exists(args.output+fname+".png"):
+            i += 1
+            fname = default_fname + f"-({i})"
+
+        return args.output + fname
+    return args.output
+
+
+
+def generateplot(args, filename, errors):
+    import matplotlib.pyplot as plt
+    plt.plot(range(args.steps), errors)
+    plt.xlabel("Timestep")
+    plt.ylabel("Error")
+    plt.title(f"Error for {args.model.__name__} ({args.data._n}-links)")
+    plt.savefig(filename+".png")
+    plt.close()
+
+
 
 
 if __name__ == "__main__":
@@ -103,5 +152,6 @@ if __name__ == "__main__":
     parser.add_argument('model', type=DynamicLoad("models"), help='model to load')
     parser.add_argument('weight', type=latest_file, help='model weight to load')
     parser.add_argument('steps', type=int, help="number of steps to evaluate over")
+    parser.add_argument('output', type=str, help="output file to save to")
 
     main(parser.parse_args())
