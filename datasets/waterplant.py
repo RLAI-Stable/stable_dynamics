3291a4ba0db065b9dc1ec44104c8625f80818242
Pedro Henrique Pons Fiorentin <pons@cedar1.cedar.computecanada.ca>
2023-09-25 08:20:15 -0700

    Slight refactoring

--- datasets/waterplant.py
+++ datasets/waterplant.py
@@ -10,7 +10,6 @@ TEST_CACHE = Path("test_nov22_no_trace.pkl")
 NUM_EXAMPLES = 200
 def build(props):
     DATA_FILE = TEST_CACHE if "test" in props else TRAIN_CACHE
-    print(DATA_FILE)
     with open(DATA_FILE, 'rb') as file:
         dataset = pickle.load(file)
 
@@ -21,9 +20,7 @@ def build(props):
         X, Y = torch.tensor(X), torch.tensor(Y)
         X, Y = X.to(torch.float32), Y.to(torch.float32)
         rv = torch.utils.data.TensorDataset(X, Y)
-        print(rv)
         return rv
     
 if __name__ == "__main__":
-    print("bom dia meninas")
     build([])
