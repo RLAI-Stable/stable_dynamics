2269be786eb632a75ac6fd14a7750e8ce3b07a89
phpons <ph.pfiorentin@gmail.com>
2023-10-09 22:03:35 +0200

    N-STEP data loading

--- datasets/waterplant.py
+++ datasets/waterplant.py
@@ -4,21 +4,28 @@ import numpy as np
 import pickle
 import torch
 
-TRAIN_CACHE = Path("train_nov22_no_trace.pkl")
-TEST_CACHE = Path("test_nov22_no_trace.pkl")
+TRAIN_FILE = Path("train_nov22_no_trace.pkl")
+TEST_FILE = Path("test_nov22_no_trace.pkl")
 
-NUM_EXAMPLES = 200
+SENSOR_INDEX = 53
+N = 10
+
 def build(props):
-    DATA_FILE = TEST_CACHE if "test" in props else TRAIN_CACHE
+    DATA_FILE = TEST_FILE if "test" in props else TRAIN_FILE
     with open(DATA_FILE, 'rb') as file:
         dataset = pickle.load(file)
 
-        X = dataset[:-1]
-        Y = dataset[1:]
+        # Extract X and Y
+        X = np.array(dataset.iloc[:-N])  # excluding the last N rows (N-step)
+        Y = np.zeros_like(X)             # Y is .zeros() since we only care about one sensor in prediction
 
-        X, Y = np.array(X), np.array(Y)
+        # Populate Y with the specified conditions
+        for i in range(len(Y)):
+            Y[i, SENSOR_INDEX] = np.sum(dataset.iloc[i:i+N, SENSOR_INDEX])
+
         X, Y = torch.tensor(X), torch.tensor(Y)
         X, Y = X.to(torch.float32), Y.to(torch.float32)
+
         rv = torch.utils.data.TensorDataset(X, Y)
         return rv
     
