ca1f7c7a9eb71f089f1626417f46cc77bf946350
Pedro Pons <pdropons@amazon.nl>
2024-02-06 16:40:31 +0100

    Save rollout figures

--- rollout_eval.py
+++ rollout_eval.py
@@ -9,9 +9,10 @@ import torch
 from util import (DynamicLoad, latest_file, loadDataFile, setup_logging, to_variable)
 
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 PIT300_COLUMN = "ZW_PIT300.DATA"
-SKIP=20
+PIT300_INDEX = 53
 
 logger = setup_logging(os.path.basename(__file__))
 
@@ -22,30 +23,39 @@ def main(args):
     if torch.cuda.is_available():
         model.cuda()
 
-    h = args.skip
+    all_sensors_data = load_water_data()
+    PIT300_data = list(all_sensors_data[PIT300_COLUMN])
 
-    data = load_water_data()
-    sensor_data = list(data[PIT300_COLUMN])
-    current_step = get_starting_point(data)
+    current_step = get_starting_point(all_sensors_data)
+    starting_PIT300 = current_step[0][PIT300_INDEX].item()
 
-    X_nn = current_step
     total_error = 0
+    predicted_rollout = [starting_PIT300]
+    true_rollout = [starting_PIT300]
     for i in range(1, args.steps):
-        current_step = predict_next_step(model, h, X_nn)
-        total_error += calculate_prediction_error(current_step.cpu().numpy(), sensor_data, i, skip=SKIP)
+        current_step = predict_next_step(model, current_step)
+
+        current_PIT300 = current_step[0][PIT300_INDEX].cpu().numpy()
+        true_PIT300 = PIT300_data[i * args.skip]
+        
+        predicted_rollout.append(current_PIT300)
+        true_rollout.append(true_PIT300)
+
+        total_error += calculate_prediction_error(current_PIT300, true_PIT300)
 
     save_error(args.save_to, total_error)
+    save_rollout(args.save_image_to, predicted_rollout, true_rollout, total_error)
 
 
-def predict_next_step(model, h, X_nn):
+def predict_next_step(model, X_nn):
     # TODO: RK4 could be interesting here
     X_nn.requires_grad = True
     k1 = model(X_nn) # H can be 1
     k1 = k1.detach()
     return k1
 
-def calculate_prediction_error(model_prediction, sensor_data, step, skip=20):
-    return abs(sensor_data[step * skip] - model_prediction[0][53].item())
+def calculate_prediction_error(model_prediction, true_data):
+    return abs(true_data - model_prediction)
 
 def load_water_data():
     cache_path = Path("sensor-cache") / f"test_nov22_no_trace.pkl"
@@ -62,11 +72,29 @@ def save_error(path, total_error):
     with open(path, 'wb') as f:
         pickle.dump(rollout_error, f)
 
+def save_rollout(path, predicted_rollout, true_rollout, total_error):
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(predicted_rollout, label='Model predictions')
+    plt.title('Model Predictions (Skip 10, stable)')
+              
+    plt.subplot(1, 2, 2)
+    plt.plot(true_rollout, label='PIT300')
+    plt.title('PIT300 - Test Set')
+
+    plt.tight_layout()
+
+    # Show the plots
+    plt.savefig(path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Error of .')
-    parser.add_argument('--skip', type=float, default=20, help="Downscaling of the rollout of data")
+    parser.add_argument('--skip', type=int, default=20, help="Downscaling of the rollout of data")
     parser.add_argument('--save-to', type=str, help="Destination of the file containing evaluation loss")
+    parser.add_argument('--save-image-to', type=str, help="Destination of the file containing the predicted rollout plot")
+
 
     parser.add_argument('model', type=DynamicLoad("models"), help='model to load')
     parser.add_argument('weight', type=latest_file, help='model weight to load')
