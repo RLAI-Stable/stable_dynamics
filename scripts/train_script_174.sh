0e3ff09e79f9ec692634b72da52b79b0a7ef8ba2
phpons <ph.pfiorentin@gmail.com>
2024-04-17 22:42:29 -0300

    Training scripts

--- scripts/train_script_174.sh
+++ scripts/train_script_174.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./train_water_stable "stable" "0.001" "PSD-REHU" "0.01" "0" "0.005" "1" "20" "0.0003" "300" "128" "./water_data/test_12m_april2023_128_sensors.pkl" "./water_data/test_12m_july2023_128_sensors.pkl" "./water_data/test_12m_july2023_128_sensors.pkl"
+./train_water_stable "stable" "0.001" "PSD-REHU" "0.01" "0" "0.005" "1" "20" "0.0005" "300" "128" "./water_data/train_july2022_128_sensors.pkl" "./water_data/test_12m_july2023_128_sensors.pkl" "./water_data/test_12m_july2023_128_sensors.pkl"
